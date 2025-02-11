25aeeb16285829207445f28682593adad96ec1c1
Hammad Ullah Khan <hammadkhan.dc@gmail.com>
2025-02-11 15:30:06 +0500

    Create StudentDashboard.jsx

---
+++ frontend/src/components/dashboards/StudentDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StudentDashboard = () => {
+  return (
+    <div>
+      <h1>🎒 Student Dashboard</h1>
+      <p>Track your progress here!</p>
+    </div>
+  );
+};
+
+export default StudentDashboard;
